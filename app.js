c1b6d3f2727a529b3caa09a2d9a61cb4a0dbdde6
iockat <githubiockat.dismount651@passmail.com>
2026-03-12 14:09:24 +0100

    updates

--- app.js
+++ app.js
@@ -7,27 +7,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 8080;
-
-// In-memory storage of last 100 events
-const eventHistory = [];
-const MAX_HISTORY = 100;
-
 // Connected SSE clients
 let clients = [];
 
 // SSE endpoint
-app.get("/events/stream", (req, res) => {
+app.get("/stream", (req, res) => {
   res.set({
     "Content-Type": "text/event-stream",
     "Cache-Control": "no-cache",
     Connection: "keep-alive",
   });
   res.flushHeaders();
-
-  // Send last events on connect
-  eventHistory.forEach((event) =>
-    res.write(`data: ${JSON.stringify(event)}\n\n`),
-  );
 
   clients.push(res);
 
@@ -36,22 +26,11 @@ app.get("/events/stream", (req, res) => {
   });
 });
 
-// Historical events endpoint
-app.get("/events", (req, res) => {
-  res.json({ events: eventHistory });
-});
-
-// Receive new events from n8n
-app.post("/events", (req, res) => {
+// Receive new events
+app.post("/stream", (req, res) => {
   const event = req.body;
-
-  // Add to history
-  eventHistory.push(event);
-  if (eventHistory.length > MAX_HISTORY) eventHistory.shift();
-
   // Push to all connected SSE clients
   clients.forEach((c) => c.write(`data: ${JSON.stringify(event)}\n\n`));
-
   res.sendStatus(200);
 });
 
